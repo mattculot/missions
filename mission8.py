3479f215ceab6ca19256cf0d5007cc9eafde36df
zebi3619 <curatilivio@gmail.com>
2025-11-18 17:58:23 +0100

    Add files via upload

--- mission8.py
+++ mission8.py
@@ -9,11 +9,11 @@ class Duree :
         self.h = h
         self.m = m
         self.s = s
-        if self.s > 60:
+        while self.s > 60:
             self.m += 1
-        if self.m > 60:
+        while self.m > 60:
             self.h += 1
-        pass
+        
 
     def to_secondes(self):
         
@@ -24,10 +24,6 @@ class Duree :
         compte_sec_tot = self.s + (self.m*60) + (self.h*3600)
         return compte_sec_tot
     
-    def  afficher_secondes(self):
-        compte_sec_tot = self.to_secondes()
-        print("une durée de " + str(self.h) + "h " + str(self.m) + "m " + str(self.s) + "s compte " + str(compte_sec_tot) + " secondes")
-
     def delta(self,d) :
         """
         @pre:  d est une instance de la classe Duree
@@ -41,24 +37,23 @@ class Duree :
         nbr_sec_d = d.to_secondes()
         compte_sec_tot = self.s + (self.m*60) + (self.h*3600)
         diff = compte_sec_tot - nbr_sec_d
+        print(diff)
         return diff
     
-    def afficher_delta(self, d):
-        diff = self.delta(d)
-        print(str(diff) + " secondes")
-        
+    def afficher_seconde(self):
+        compte_sec_tot = to_secondes(self)
+        print("une durée de " + str(self.h) + "h " + str(self.m) + "m " + str(self.s) + "s compte " + str(compte_sec_tot) + " secondes")
+
     def apres(self,d):
         """
         @pre:  d est une instance de la classe Duree
         @post: Retourne True si cette durée (self) est plus grand que la durée
                d passée en paramètre; retourne False sinon.
         """
-        diff = self.delta(d)
         if diff <= 0:
             return False
         else:
             return True
-        
     def ajouter(self,d):
         """
         @pre:  d est une instance de la classe Duree
@@ -69,6 +64,14 @@ class Duree :
                (60 secondes = 1 minute, 60 minutes = 1 heure).
                Ne retourne pas une nouvelle durée mais modifié la durée self.
         """
+        self.d = d
+        self.h = self.h + self.d.h
+        self.m = self.m + self.d.m
+        while self.m > 60:
+            self.h += 1
+        self.s = self.s + self.d.s
+        while self.s > 60:
+            self.s += 1
 
     def __str__(self):
         """
@@ -78,11 +81,30 @@ class Duree :
         retourne le string désiré avec les nombres en deux chiffres en ajoutant
         les zéros nécessaires.
         """
- 
-
+        temps = "{:02}:{:02}:{:02}".format(self.h, self.m, self.s)
+        return temps
+    pass
 class Chanson :
     
-    # A COMPLETER PAR LES ETUDIANTS    
+    def __init__(self,t,a,d):
+        """
+        @pre:  t et a sont des string, d est une instance de la classe Duree
+        @post: Crée une nouvelle chanson, caractérisée par un titre t,
+        un auteur a et une durée d.
+        """
+        self.t = t
+        self.a = a
+        self.duree = d
+
+    def __str__(self):
+        """
+        @pre:  -
+        @post: Retourne un string décrivant cette chanson sous le format
+        "TITRE - AUTEUR - DUREE".
+        Par exemple: "Let's_Dance - David_Bowie - 00:04:05"
+        """
+        descrip = self.t + " - " + self.a + " - " + self.duree.d
+        return descip
     pass
 
 class Album :
